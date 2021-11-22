00716c239530fa478d3a29e73f4bfd9b101f0d64
ahnhyunwooo <dksgusdnx@naver.com>
2021-11-22 19:47:11 +0900

    시작

---
+++ src/main/java/project/demo/LibrayApplication.java
@@ -0,0 +1,13 @@
+package project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrayApplication.class, args);
+	}
+
+}
