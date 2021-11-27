e9a5a753fde51c66a487290229f51b9d79e13836
qowldud95 <girlwldud@naver.com>
2021-11-27 11:32:14 +0900

    비밀번호 체크 기능 추가

--- src/main/resources/static/js/memberJoin.js
+++ src/main/resources/static/js/memberJoin.js
@@ -1,6 +1,8 @@
 $(document).ready(function (){
     $("#member_join_btn").click(function() {
         alert("버튼1을 누르셨습니다.");
+        console.log("dddd");
+        //pwCheck();
     });
 });
 function idOverlap() {
@@ -28,7 +30,7 @@ function idOverlap() {
             }
         },
         error : function(XMLHttpRequest, textStatus, errorThrown){
-            alert("통신 실패.")
+            alert("통신 실패.");
         }
     });
 }
@@ -36,7 +38,23 @@ function idValueCheck(id) {
     var regExp = /^[a-z]+[a-z0-9]{5,19}$/g;
     return regExp.test(id);
 }
+function pwValueCheck(pw) {
+    var regExp = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
+    return regExp.test(pw);
+}
+function pwCheck(){
+    var pw = $("#member_join_pw").val();
+    var pwCheck = $("#member_join_pw_check").val();
 
+    if(!pwValueCheck(pw)){
+        alert("최소 8 자, 하나 이상의 문자와 하나의 숫자로 입력하세요.");
+        $("#member_join_pw").css('border', '2px solid red');
+        return ;
+    }
+    if(pw != pwCheck){
+        alert("비밀번호확인란 체크.");
+    }
+}
 
 
 
