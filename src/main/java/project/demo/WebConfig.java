fe3da7f103751eee23cb6e8ea81c59cf4184586b
qowldud95 <girlwldud@naver.com>
2021-12-15 23:14:41 +0900

    로그인 인터셉터 추가

---
+++ src/main/java/project/demo/WebConfig.java
@@ -0,0 +1,27 @@
+package project.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import project.demo.argumentResolver.LoginMemberArgumentResolver;
+import project.demo.interceptor.LoginCheckInterceptor;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")  //추후 수정
+                .excludePathPatterns("/login", "/css/**");
+
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+}
