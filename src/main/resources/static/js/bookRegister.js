06408a3c7bb412b91ebb7f54a99004b59284816b
ahnhyunwooo <dksgusdnx@naver.com>
2021-12-22 23:38:12 +0900

    도서 사진 추가 삭제

--- src/main/resources/static/js/bookRegister.js
+++ src/main/resources/static/js/bookRegister.js
@@ -1,3 +1,27 @@
-function fileNameChange() {
-    let fileText =
+let fileIndex = 0;
+function fileNameChange(file) {
+    let splitFile = file.value.split('\\');
+    $("#book_register_text0").val(splitFile[splitFile.length-1]);
 }
+
+/**
+ * 파일 행 추가
+ */
+function fileRowAdd() {
+    ++fileIndex;
+    const fileHtml ='<div class="book_register_form_container">'+
+        '&nbsp;<input type="text" id="book_register_text'+fileIndex+'"class="book_register_file_text plus_file" readonly>'+
+        ' <label for="file_'+fileIndex+'" class="book_register_file_btn">파일추가</label>'+
+        ' <input type="file" id="file_'+fileIndex+'" name="file" class="book_register_file" onchange="fileNameChange(this)">'+
+        ' <button type="button" class="book_register_file_add_btn" onclick="fileRowAdd()">+</button>'+
+        ' <button type="button" class="book_register_file_add_btn" onclick="fileRowMinus(this)">-</button>';
+    $("#book_register_form").append(fileHtml);
+}
+/**
+ * 파일 행 제거
+ */
+function fileRowMinus(btn) {
+    $(btn).parent().remove();
+    console.log($(btn).parent());
+}
+
