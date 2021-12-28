d3f8f559017f70a74b035960c8a51c819c484ac6
ahnhyunwooo <dksgusdnx@naver.com>
2021-12-28 16:52:42 +0900

    책 등록 수정

--- src/main/resources/static/js/bookRegister.js
+++ src/main/resources/static/js/bookRegister.js
@@ -7,35 +7,31 @@ let fileIndex = 0;
 
 function fileNameChange(file) {
     let splitFile = file.value.split('\\');
-    $("#book_register_text0").val(splitFile[splitFile.length-1]);
+    $(file).prevAll("input[type=text]").val(splitFile[splitFile.length-1]);
 }
 
 /**
  * 파일 행 추가
  */
-
-
 function fileRowAdd(btn) {
     if($('#fileIndexTemp').val() > fileIndex){
         fileIndex = $('#fileIndexTemp').val();
     }
     ++fileIndex;
     const fileHtml ='<div class="book_register_form_container">'+
-        '&nbsp;<input type="text" id="book_register_text'+fileIndex+'"class="book_register_file_text plus_file" readonly>'+
+        ' <input type="text" id="book_register_text'+fileIndex+'"class="book_register_file_text plus_file" readonly>'+
         ' <label for="file_'+fileIndex+'" class="book_register_file_btn">파일추가</label>'+
         ' <input type="file" id="file_'+fileIndex+'" name="file" class="book_register_file" onchange="fileNameChange(this)">'+
-        ' <button type="button" class="book_register_file_add_btn" onclick="fileRowAdd()">+</button>'+
+        ' <button type="button" class="book_register_file_add_btn" onclick="fileRowAdd(this)">+</button>'+
         ' <button type="button" class="book_register_file_add_btn" onclick="fileRowMinus(this)">-</button>';
 
     $(btn).parent().parent().append(fileHtml);
     $('#fileIndexTemp').val(fileIndex);
-
 }
 /**
  * 파일 행 제거
  */
 function fileRowMinus(btn) {
     $(btn).parent().remove();
-    console.log($(btn).parent());
 }
 
